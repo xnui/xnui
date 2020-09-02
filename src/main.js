208a09f8b6cd4f874d41d9fe88e21244f6d48ac8
xn213 <812725903@qq.com>
2020-09-02 14:31:28 +0800

    feat(main.js): lib packages
    打包后, 使用打包的组件库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import xnUi from './packages'
+// import xnUi from './packages'
+import xnUi from '../dist/xnui.umd.min.js'
+import '../dist/xnui.css'
 
 Vue.config.productionTip = false
 
