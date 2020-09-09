328dbc3b7ae35c6d46a50216216b35bb36c8f0ee
xn213 <812725903@qq.com>
2020-09-09 17:33:35 +0800

    docs(doc/btn/test4): test button icon-position

--- examples/documents/docs/.vuepress/components/button/test4.vue
+++ examples/documents/docs/.vuepress/components/button/test4.vue
@@ -3,7 +3,7 @@
     <xn-button-group>
       <xn-button icon="arrow-left">上一页</xn-button>
       <xn-button icon="arrow-right"
-                 position="right">下一页</xn-button>
+                 icon-position="right">下一页</xn-button>
     </xn-button-group>
 
     <xn-button-group>
