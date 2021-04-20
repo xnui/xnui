4719869a4769e204c541ffd27284974235d238d9
xn213 <812725903@qq.com>
2021-04-20 12:59:51 +0800

    feat(App): add 唤醒pc 按钮

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <button class="clip" @click="openApp">唤醒app</button>
+
     <xn-button>默认按钮</xn-button>
     <xn-button type="primary">主要按钮</xn-button>
     <xn-button type="success">成功按钮</xn-button>
@@ -35,6 +37,41 @@
 export default {
   name: "App",
   components: {},
+  methods: {
+    openApp() {
+      console.log('aaa', '000')
+
+      this.callapp_PC({
+        // 这个url地址换成需要唤醒的客户端的URL Protoco
+        // 客户端自定义URL Protoco的设置请看我的博客 pc前端js调起电脑本地应用程序(需要客户端配合 自定义URL Protocol 协议 )
+        url: "baijiacloud://urlpath=https%3A%2F%2Fb41568785.at.baijiayun.com",
+        callback: function() {
+          console.log("唤醒失败了,做一些唤醒失败后的操作吧");
+        },
+      });
+    },
+    callapp_PC({ url, callback }) {
+      var t = setTimeout(callback, 1000);
+      var inp = document.createElement("input");
+      inp.style.position = "absolute";
+      inp.style.clip = "rect(0, 0, 0, 0)";
+      function blur() {
+        window.clearTimeout(t);
+      }
+      inp.addEventListener("blur", blur); // 监听blur事件
+      document.body.appendChild(inp);
+      inp.focus(); // 获取焦点
+      setTimeout(function() {
+        // 删除无用的标签
+        inp.removeEventListener("blur", blur);
+        document.body.removeChild(inp);
+      }, 1000);
+      //有客户端 如果有本地exe应用，就会弹框，input失去焦点，然后执行blur()事件 清空t定时器 - 删除无用的标签定时器
+      //无客户端 不会弹框，input也不会失去焦点 触发t定时器- 执行callback - 删除无用的标签定时器
+      // https://blog.csdn.net/weixin_44309374?orderby=UpdateTime
+      location.href = url;
+    },
+  },
 };
 </script>
 
